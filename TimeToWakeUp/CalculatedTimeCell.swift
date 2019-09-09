98c49ac492b75baa7c8ea31a9b8bff82ae453ecf
Alexandr Kamyshev <sanek655@mail.ru>
2019-09-09 21:44:08 +0700

    Add table view cell for calculated time

---
+++ TimeToWakeUp/CalculatedTimeCell.swift
@@ -0,0 +1,24 @@
+//
+//  EstimatedTimeCell.swift
+//  TimeToWakeUp
+//
+//  Created by Alexandr on 09/09/2019.
+//  Copyright © 2019 Alexandr. All rights reserved.
+//
+
+import UIKit
+
+class CalculatedTimeCell: UITableViewCell {
+
+    @IBOutlet weak var leadTimeLabel: UILabel!
+    @IBOutlet weak var trailingInfoLabel: UILabel!
+    
+    override func awakeFromNib() {
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
